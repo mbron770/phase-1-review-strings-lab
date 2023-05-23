772eea6d508173ece69ecb8a86e5106aaa14bd23
mbron770 <mbronfin.tsd@gmail.com>
2023-05-22 21:39:49 -0400

    add strings

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code in this file!
+
+
+const currentUser = 'Grace Hopper' 
+
+const welcomeMessage1 = 'Welcome to Flatbook'
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`; 
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting1 = `Welcome, ${currentUser}`; 
+
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
